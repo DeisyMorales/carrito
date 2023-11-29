d8e17b5b60763c98d3af5b2d5711743ac1ad4cfc
DeisyMorales <deysimorales0102@gmail.com>
2023-11-29 12:02:58 -0500

    enviar

--- include/modal_frm_reg_usuario.php
+++ include/modal_frm_reg_usuario.php
@@ -1,6 +1,6 @@
-<button type="button" class="btn btn-success waves-effect waves-light" data-toggle="modal" data-target=".usuario">+ Nuevo</button>
+<button type="button" class="btn btn-success waves-effect waves-light" data-toggle="modal" data-target=".registrar">+ Nuevo</button>
 
-<div class="modal fade usuario" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade registrar" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                                         <div class="modal-dialog modal-lg">
                                             <div class="modal-content">
                                                 <div class="modal-header">
