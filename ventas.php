be80579fadeeed388d442c1922be596df2b0b784
DeisyMorales <deysimorales0102@gmail.com>
2023-12-17 21:40:44 -0500

    enviar

--- ventas.php
+++ ventas.php
@@ -1,116 +1,101 @@
 <?php
-include("include/conexion.php");
+include('include/conexion.php');
 ?>
 
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas</title>
-    <link rel="stylesheet" href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" type="text/css" >
+    <link rel="stylesheet" href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" type="text/css">
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 
-      <!-- Plugins css -->
-      <link href="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <!-- Plugins css -->
+    <link href="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
 
 </head>
+
+
 <body>
-
-
-<?php 
-//lenguaje en php
-include ("include/menu.php");
-?>
-
-<!--Inicio de Contenido-->
-<div class="main-content">
-    <div class="page-content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-              
-                    <h4 class="">Registro de Ventas</h4>
-                    <a href="venta.php" class="btn btn-success">+ Nuevo</a>
-                    <div class="card">
-                        <div class="card-body">
-                        <table id="basic-datatable" class="table dt-responsive nowrap">
-                                <thead>
-                                    <tr>
-                                        <th>Nro</th>
-                                        <th>Serie de venta</th>
-                                        <th>Numero de venta</th>
-                                        <th>Fecha de venta</th>
-                                        <th>Monto total</th>
-                                        <th>id cliente</th>
-                                        <th>id usuario</th>
-
-                                        <th>Acciones</th>
-                                        
-                                       
-                                       
-
-                                    </tr>
+    <?php
+    // lenguaje en php
+    include("include/menu.php");
+    ?>
+    <!-- INICIO DE CONTENIDO -->
+    <div class="main-content">
+        <div class="page-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Relación de Ventas</h4>
+                        <a href="venta.php" class="btn btn-success">+ Nuevo</a>
+                        <div class="card">
+                            <div class="card-body">
+                                
+                                <table id="basic-datatable" class="table dt-responsive nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th>Nro</th>
+                                            <th>Foto</th>
+                                            <th>DNI</th>
+                                            <th>Apellidos y Nombres</th>
+                                            <th>Correo</th>
+                                            <th>Telefono</th>
+                                            <th>Direccion</th>
+                                            <th>Estado</th>
+                                            <th>Rol</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
                                     <tbody>
-                                    <?php
-                                        $consulta ="SELECT * FROM ventas";
-                                        $ejecutar =mysqli_query($conexion,$consulta);
-                                        $contador =0;
-                                        while ($respuesta =mysqli_fetch_array($ejecutar)) {
-                                            # code...
+                                        <?php
+                                        $consulta = "SELECT * FROM usuario";
+                                        $ejecutar = mysqli_query($conexion, $consulta);
+                                        $contador = 0;
+                                        while ($respuesta = mysqli_fetch_array($ejecutar)) {
                                             $contador += 1;
-                                            echo "<tr>";
-                                            echo "<td>".$contador."</td>";
-                                            echo "<td>".$respuesta['serie_venta']."</td>";
-                                            echo "<td>".$respuesta['numero_venta']."</td>";
-                                            echo "<td>".$respuesta['fecha_hora_venta']."</td>";
-                                            echo "<td>".$respuesta['monto_total']."</td>";
-                                            echo "<td>".$respuesta['id_cliente']."</td>";
-                                            echo "<td>".$respuesta['id_usuario']."</td>";
-                                           
-                                            
-                                            echo "<td><button class='btn btn-primary'>Editar</button><button class='btn btn-danger'>Eliminar</button></td>";
-                                            
-                                            echo "</tr>";
+                                           echo "<tr>";
+                                           echo "<td>".$contador."</td>";
+                                           echo "<td>".$respuesta['foto']."</td>";
+                                           echo "<td>".$respuesta['dni']."</td>";
+                                           echo "<td>".$respuesta['apellidos_nombres']."</td>";
+                                           echo "<td>".$respuesta['correo']."</td>";
+                                           echo "<td>".$respuesta['telefono']."</td>";
+                                           echo "<td>".$respuesta['direccion']."</td>";
+                                           echo "<td>".$respuesta['activo']."</td>";
+                                           echo "<td>".$respuesta['id_rol']."</td>";
+                                           echo "<td><button class='btn btn-success'>Editar</button><button class='btn btn-danger'>Eliminar</button></td>";
+                                           echo "</tr>";
                                         }
-                                        
-                                        
                                         ?>
-                                     
-
                                     </tbody>
-                                </thead>
-                            </table>
-                       
+                                </table>
+
+                            </div>
                         </div>
-                  
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!--Fin de Contenido-->
+    <!-- FIN DE CONTENIDO -->
 
-
-
-     <!-- jQuery  -->
+    <!-- jQuery  -->
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
 
-     <!-- third party js -->
-     <script src="plantilla/Admin/plugins/datatables/jquery.dataTables.min.js"></script>
+    <!-- third party js -->
+    <script src="plantilla/Admin/plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.js"></script>
     <script src="plantilla/Admin/plugins/datatables/dataTables.responsive.min.js"></script>
     <script src="plantilla/Admin/plugins/datatables/responsive.bootstrap4.min.js"></script>
@@ -127,10 +112,7 @@ include ("include/menu.php");
 
     <!-- Datatables init -->
     <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
-
-
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
-
 </body>
 </html>
