d9ce98baba88affc22e159c197a8807513ef6622
DeisyMorales <deysimorales0102@gmail.com>
2023-12-18 08:54:47 -0500

    enviar

--- venta.php
+++ venta.php
@@ -208,6 +208,17 @@ include ("include/menu.php");
 
         };
         function eliminar_producto(id){
+            
+            $.ajax({
+                type: "POST",
+                url: "operaciones/eliminar_producto_tabla.php",
+                data: {
+                    id_p: id
+                },
+                success: function(r) {
+                    $('#contenido_tabla').html(r);
+                }
+            })
 
         }
     </script>
